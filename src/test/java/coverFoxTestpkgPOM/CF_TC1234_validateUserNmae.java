562cf1782e4773b832f161a615d274c45fb41016
RITESH-MESHRAM <riteshpm1998@gmail.com>
2024-08-18 23:40:30 +0530

    [Ritesh]my commit

---
+++ src/test/java/coverFoxTestpkgPOM/CF_TC1234_validateUserNmae.java
@@ -0,0 +1,14 @@
+package coverFoxTestpkgPOM;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CF_TC1234_validateUserNmae {
+
+	
+	@Test
+	public void validateUserName() {
+		Reporter.log("This is validate user name class",true);
+	}
+	
+}
